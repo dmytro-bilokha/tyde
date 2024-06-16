8f2694beffbcfd3c35e1d2e4b2135a0d7b643956
Dmytro Bilokha <dmytro@posteo.net>
2024-06-16 18:22:18 +0200

    Remove servlets used for authentication/authorization testing, remove dummy roles usage

--- backend/src/main/java/com/dmytrobilokha/tyde/user/AuthenticationConstants.java
+++ backend/src/main/java/com/dmytrobilokha/tyde/user/AuthenticationConstants.java
@@ -5,9 +5,6 @@ public final class AuthenticationConstants {
     public static final String REMEMBER_ME_COOKIE_NAME = "JREMEMBERMEID";
     public static final int REMEMBER_ME_COOKIE_LIFE = 60 * 60 * 24 * 24; // 24 days.
     public static final String LOGOUT_SERVLET_PATH = "/logout";
-    public static final String LOGIN_SERVLET_PATH = "/login";
-    public static final String LOGIN_ERROR_PAGE_PATH = "/login-error.xhtml";
-    public static final String DEFAULT_AFTER_LOGIN_PATH = "/usertest";
     public static final String DEFAULT_AFTER_LOGOUT_PATH = "/";
 
     private AuthenticationConstants() {
